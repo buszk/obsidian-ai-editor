2db3f56b9eab6161a5809f5cbe0aa8c5233a1fd6
Zekun Shen <bruceshenzk@gmail.com>
2023-08-13 21:16:25 -0400

    Working TL;DR example

---
+++ config.ts
@@ -0,0 +1,6 @@
+export enum Mode {
+    PROD,
+    TEST,
+}
+
+export let mode = Mode.PROD;
--- main.ts
+++ main.ts
@@ -1,81 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { textCompletion } from 'llm';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface AIEditorSettings {
+	openAiApiKey: string;
+	summaryHeading: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AIEditorSettings = {
+	openAiApiKey: '',
+	summaryHeading: '**TL;DR**: '
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AIEditor extends Plugin {
+	settings: AIEditorSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		let statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('AI');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
+			id: 'ai-editor-tldr',
+			name: 'TLDR command',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const apiKey = this.settings.openAiApiKey;
+				if (!apiKey) {
+					new Notice("API key is not set in plugin settings");
+					return;
 				}
+				const prompt = "Summarize the following in a paragraph";
+				const wholeText = editor.getValue()
+				new Notice("Generating summary")
+				let res = "";
+				try {
+					res = await textCompletion(prompt, wholeText, apiKey)
+				} catch (error) {
+					console.error("Error calling text completion API:", error);
+					new Notice("Error generating text. Please check the plugin console for details.")
+					return;
+				}
+
+				res = this.settings.summaryHeading + res + "\n"
+				editor.setCursor(0, 0)
+				editor.replaceRange(res, editor.getCursor())
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new AIEditorSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,43 +69,27 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class AIEditorSettingTab extends PluginSettingTab {
+	plugin: AIEditor;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AIEditor) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('OpenAI API Key')
+			.setDesc('OpenAI API Key')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API key')
+				.setValue(this.plugin.settings.openAiApiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.openAiApiKey = value;
 					await this.plugin.saveSettings();
 				}));
 	}
