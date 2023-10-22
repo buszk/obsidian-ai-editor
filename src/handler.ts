a85f125db8f136335190c7ca148896fc2b9e306a
Zekun Shen <bruceshenzk@gmail.com>
2023-10-21 23:50:51 -0400

    Rewrite with switch

--- src/handler.ts
+++ src/handler.ts
@@ -21,30 +21,36 @@ export class ActionHandler {
 	}
 
 	getTextInput(sel: Selection, editor: Editor) {
-		if (sel == Selection.ALL) {
-			return editor.getValue();
-		} else if (Selection.CURSOR) {
-			return editor.getSelection();
-		} else {
-			console.log(`Selection ${sel}`);
-			throw "Selection not implemented";
+		switch (sel) {
+			case Selection.ALL:
+				return editor.getValue();
+			case Selection.CURSOR:
+				return editor.getSelection();
+			default:
+				console.log(`Selection ${sel}`);
+				throw "Selection not implemented";
 		}
 	}
 
 	addToNote(loc: Location, text: string, editor: Editor) {
-		if (loc == Location.INSERT_HEAD) {
-			editor.setCursor(0, 0);
-			editor.replaceRange(text, editor.getCursor());
-		} else if (loc == Location.APPEND_BOTTOM) {
-			editor.setCursor(editor.lastLine());
-			editor.replaceRange(text, editor.getCursor());
-		} else if (loc == Location.APPEND_CURRENT) {
-			text = editor.getSelection() + text;
-			editor.replaceSelection(text);
-		} else if (loc == Location.REPLACE_CURRENT) {
-			editor.replaceSelection(text);
-		} else {
-			throw "Location not implemented";
+		switch (loc) {
+			case Location.INSERT_HEAD:
+				editor.setCursor(0, 0);
+				editor.replaceRange(text, editor.getCursor());
+				break;
+			case Location.APPEND_BOTTOM:
+				editor.setCursor(editor.lastLine());
+				editor.replaceRange(text, editor.getCursor());
+				break;
+			case Location.APPEND_CURRENT:
+				text = editor.getSelection() + text;
+				editor.replaceSelection(text);
+				break;
+			case Location.REPLACE_CURRENT:
+				editor.replaceSelection(text);
+				break;
+			default:
+				throw "Location not implemented";
 		}
 	}
 
